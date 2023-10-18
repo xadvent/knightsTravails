3c65f2c052c926cb0bc530c41cfad95ee58a1132
xadvent <piercestrutt@gmail.com>
2023-10-18 14:12:13 -0600

    Created basic webpack config and added dist and src folders

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    devtool: "inline-source-map",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Knight Travails',
+            sourceMap: true,
+            template: './src/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /\.(csv|tsv)$/i,
+            //     use: ['csv-loader'],
+            // },
+            // {
+            //     test: /\.xml$/i,
+            //     use: ['xml-loader'],
+            // },
+        ]
+    },
+}
